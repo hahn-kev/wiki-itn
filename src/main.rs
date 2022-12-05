6fc752bcd6e0f07b3a9eacd8ed9938b5b4b3f0ba
Kevin Hahn <hahn.kev@gmail.com>
2022-12-05 20:49:21 +0700

    fix parsing due to another `mw-parser-output` class added to a div above our target

--- src/main.rs
+++ src/main.rs
@@ -117,7 +117,10 @@ fn attributes_to_string(attr: &HashMap<String, Option<String>>) -> String {
 }
 
 fn find_root_element(dom: &Dom) -> Option<&Element> {
-    let div = find_element_with_class(&dom.children, "div", &"mw-parser-output".to_string())?;
+    let body_content = find_element_with_id(&dom.children, "div", "body_content")
+        .expect("unable to find div id='body_content'");
+    let div = find_element_with_class(&body_content.children, "div", &"mw-parser-output".to_string())
+        .expect("unable to find div class='mw-parser-output'");
     for node in &div.children {
         match node {
             Node::Element(e) if e.name == "ul" => return Some(e),
@@ -133,6 +136,17 @@ fn find_element_with_class<'a>(children: &'a [Node], element_name: &str, class: 
         if let Node::Element(e) = node {
             if e.name == element_name && e.classes.contains(class) { return Some(e); }
             if let Some(e) = find_element_with_class(&e.children, element_name, class) { return Some(e); }
+        }
+    }
+    None
+}
+
+#[allow(clippy::ptr_arg)]
+fn find_element_with_id<'a>(children: &'a [Node], element_name: &str, id: &str) -> Option<&'a Element> {
+    for node in children {
+        if let Node::Element(e) = node {
+            if e.name == element_name && e.id.as_ref().map_or(false, |i| i == id) { return Some(e); }
+            if let Some(e) = find_element_with_id(&e.children, element_name, id) { return Some(e); }
         }
     }
     None
